e7bd4bd14232228f736181d23ec10f38be39b947
hobbyluv <princewillchime43@gmail.com>
2020-02-06 01:16:59 +0100

    changing of background color, text and image of elements  from the state values. react.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import cart from "./component/Header/menu icon.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    image: logo,
+    replaceText: "i appeared when you clicked the buttons!!!",
+    backgroundColor: "black",
+    text: "Learn React",
+    width: ""
+  };
+
+  handleColor = () => {
+    this.setState({ backgroundColor: "purple" });
+  };
+
+  handleText = () => {
+    this.setState({ text: this.state.replaceText });
+  };
+  handleImage = () => {
+    this.setState({ image: cart, width: "150px" });
+  };
+  render() {
+    return (
+      <div style={{ backgroundColor: this.state.backgroundColor }}>
+        <header className="App-header">
+          <img
+            src={this.state.image}
+            className="App-logo"
+            alt="logo"
+            style={{ width: this.state.width }}
+          />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.state.text}
+          </a>
+          <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <button onClick={this.handleColor}>change color</button>
+            <button onClick={this.handleImage}>change image</button>
+            <button onClick={this.handleText}>change text</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
